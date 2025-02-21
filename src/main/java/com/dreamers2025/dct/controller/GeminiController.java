8d3f68bacc7b2589340c16b22e79fba505e71b36
tony24123 <tjdduswns01@naver.com>
2025-02-21 14:17:52 +0900

    프론트 주소 허용

--- src/main/java/com/dreamers2025/dct/controller/GeminiController.java
+++ src/main/java/com/dreamers2025/dct/controller/GeminiController.java
@@ -1,17 +1,18 @@
 package com.dreamers2025.dct.controller;
 import com.dreamers2025.dct.dto.request.DreamInterpretationRequest;
-import com.dreamers2025.dct.service.GeminiService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import com.dreamers2025.dct.service.GeminiService;
 
 
 @RestController
+@RequiredArgsConstructor
 public class GeminiController {
 
-    @Autowired
-    private GeminiService geminiService;
+    private final GeminiService geminiService;
 
     @GetMapping("/api/gemini/dream-interpretation")
     public String getDreamInterpretation(@ModelAttribute DreamInterpretationRequest request) {
